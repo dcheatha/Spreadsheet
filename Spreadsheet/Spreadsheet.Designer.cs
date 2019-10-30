2fc9508d9a72f190926abc70ff024841e8f149f4
D. Cheatham <7143383+dcheatha@users.noreply.github.com>
2019-10-29 22:02:35 -0700

    Set AddRows & DeleteRows to false. Programmatically create columns A to Z.

--- Spreadsheet/Spreadsheet.Designer.cs
+++ Spreadsheet/Spreadsheet.Designer.cs
@@ -28,69 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.A = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.B = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.C = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.mainDataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.mainDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // mainDataGridView
             // 
-            this.dataGridView1.AllowUserToOrderColumns = true;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.A,
-            this.B,
-            this.C});
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 72;
-            this.dataGridView1.RowTemplate.Height = 31;
-            this.dataGridView1.Size = new System.Drawing.Size(800, 450);
-            this.dataGridView1.TabIndex = 0;
-            // 
-            // A
-            // 
-            this.A.HeaderText = "A";
-            this.A.MinimumWidth = 9;
-            this.A.Name = "A";
-            this.A.Width = 175;
-            // 
-            // B
-            // 
-            this.B.HeaderText = "B";
-            this.B.MinimumWidth = 9;
-            this.B.Name = "B";
-            this.B.Width = 175;
-            // 
-            // C
-            // 
-            this.C.HeaderText = "C";
-            this.C.MinimumWidth = 9;
-            this.C.Name = "C";
-            this.C.Width = 175;
+            this.mainDataGridView.AllowUserToAddRows = false;
+            this.mainDataGridView.AllowUserToDeleteRows = false;
+            this.mainDataGridView.AllowUserToOrderColumns = true;
+            this.mainDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.mainDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainDataGridView.Location = new System.Drawing.Point(0, 0);
+            this.mainDataGridView.Name = "mainDataGridView";
+            this.mainDataGridView.RowHeadersWidth = 72;
+            this.mainDataGridView.RowTemplate.Height = 31;
+            this.mainDataGridView.Size = new System.Drawing.Size(800, 450);
+            this.mainDataGridView.TabIndex = 0;
             // 
             // Spreadsheet
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.mainDataGridView);
             this.Name = "Spreadsheet";
             this.Text = "Totally Modern Spreadsheet Application";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Load += new System.EventHandler(this.SpreadsheetLoad);
+            ((System.ComponentModel.ISupportInitialize)(this.mainDataGridView)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn A;
-        private System.Windows.Forms.DataGridViewTextBoxColumn B;
-        private System.Windows.Forms.DataGridViewTextBoxColumn C;
+        private System.Windows.Forms.DataGridView mainDataGridView;
     }
 }
 
--- Spreadsheet/Spreadsheet.cs
+++ Spreadsheet/Spreadsheet.cs
@@ -8,6 +8,7 @@ namespace Spreadsheet_D._Cheatham
 {
     #region
 
+    using System;
     using System.Runtime.InteropServices;
     using System.Windows.Forms;
 
@@ -24,11 +25,28 @@ namespace Spreadsheet_D._Cheatham
         public Spreadsheet()
         {
             SetProcessDPIAware();
-
             this.InitializeComponent();
         }
 
         [DllImport("user32.dll")]
         private static extern bool SetProcessDPIAware();
+
+        /// <summary>
+        /// Loads the spreadsheet
+        /// </summary>
+        /// <param name="sender">
+        /// Event Sender
+        /// </param>
+        /// <param name="e">
+        /// Event that is the Event
+        /// </param>
+        private void SpreadsheetLoad(object sender, EventArgs e)
+        {
+            for (var pos = 0; pos < 26; pos++)
+            {
+                var letter = char.Parse("A");
+                this.mainDataGridView.Columns.Add(pos.ToString(), ((char)(letter + pos)).ToString());
+            }
+        }
     }
 }
